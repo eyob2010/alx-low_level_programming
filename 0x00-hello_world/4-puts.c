5bc3aadb95aea383b48c7deba4ef63af7d084dfd
Eyob <e.mersha@gmail.com>
2022-06-02 21:59:16 +0000

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
+#include <stdio.h>
 /**
  * puts - used to print text, an alternate to prntff
  */
-#include<stdio.h>
-
 int main(void) 
 {
-		puts("\"Programming is like building a multilingual puzzle");
-		getchar();
-    return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	getchar(); 
+	return 0;
 }
 
