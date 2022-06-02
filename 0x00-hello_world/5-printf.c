216e22bff007b647f33061e57603cc23b9b74d03
Eyob <e.mersha@gmail.com>
2022-06-02 23:27:19 +0000

    Task 6

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ * Return: Always 0 (Success)
+ * Description: What a language
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
