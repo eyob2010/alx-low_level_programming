77dc1a5c10f0c29765ab835564ce1584cf14e3a2
Eyob <e.mersha@gmail.com>
2022-06-02 22:15:15 +0000

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,8 @@
  */
 int main(void) 
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	getchar(); 
-	return 0;
+puts("\"Programming is like building a multilingual puzzle");
+getchar(); 
+return 0;
 }
 
