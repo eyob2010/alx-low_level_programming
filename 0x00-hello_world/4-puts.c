aec8f53c27f24a2bbcb254bee33f7c446929b8ce
Eyob <e.mersha@gmail.com>
2022-06-02 22:03:33 +0000

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,11 @@
 /**
  * puts - used to print text, an alternate to prntff
  */
+/**
+ * Main -entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void) 
 {
 	puts("\"Programming is like building a multilingual puzzle");
