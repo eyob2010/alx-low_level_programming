a0733ec55903542b95d438a98ea53c143d20665c
Eyob <e.mersha@gmail.com>
2022-06-03 17:36:01 +0000

    If project task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,11 +3,12 @@
 #include <time.h>
 /* more headers goes there */
 
-/* 
- * main - Entry point
- *
- * Return: Always 0 (Success) 
- */
+/* betty style doc for function main goes there */
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
 int main(void)
 {
 		int n;
