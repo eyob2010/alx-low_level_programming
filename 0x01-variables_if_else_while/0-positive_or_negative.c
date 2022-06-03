1eda1b144b22b9e7b00ff2cfa819c6e2723539d2
Eyob <e.mersha@gmail.com>
2022-06-03 16:58:49 +0000

    If project task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,7 +16,7 @@ int main(void)
 				} else if (n>0) {
 						printf("%d is positive\n",n);
 					} else {
-						printf("%d is zero",n);
+						printf("%d  is zero",n);
 					}
 					return (0);
 }
