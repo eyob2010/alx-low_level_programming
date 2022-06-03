7232905bbcaa2645c95ccca909119764800cef15
Eyob <e.mersha@gmail.com>
2022-06-03 23:16:49 +0000

    If project task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		putchar(i + '0'), putchar(',');
+	return (0);
+}
