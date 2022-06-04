d2b19ac17ca224f6cf1457338f97ced7b5906d2b
Eyob <e.mersha@gmail.com>
2022-06-04 01:30:20 +0000

    task 9

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,13 +5,12 @@
  */
 int main(void)
 {
-	int i, start;
-	
+	int i, start;	
 	for (i = 0; i <= 99; i++)
-		for(start = i+1; start <= 9; start++)
+		for(start = i + 1; start <= 9; start++)
 			if(i < 8)
-				putchar(i + '0'), putchar(start + '0'), putchar(','), putchar(' ');
+				putchar (i + '0'), putchar (start + '0'), putchar (','), putchar (' ');
 			else
-				putchar(i + '0'), putchar(start + '0'), putchar('\n');
+				putchar (i + '0'), putchar (start + '0'), putchar ('\n');
 	return(0);
 }
