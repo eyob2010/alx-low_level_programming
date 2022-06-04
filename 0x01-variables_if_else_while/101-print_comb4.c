e465c57cb658063c37a871c1e1b50dc8f301c36c
Eyob <e.mersha@gmail.com>
2022-06-04 02:07:19 +0000

    task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,7 +11,7 @@ int main(void)
 		for (j = i + 1; j <= 9; j++)
 			for (k = j + 1; k <= 9; k++)
 				if (i < 7)
-					putchar(i + '0'), putchar(j + '0'), putchar(k + '0'), putchar (','), putchar (' ');
+					putchar(i + '0'), putchar(j + '0'), putchar(k + '0'), putchar(','), putchar(' ');
 				else
 					putchar (i + '0'), putchar (j + '0'), putchar (k + '0'), putchar ('\n');
 	return (0);
