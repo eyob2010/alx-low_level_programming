7a1f68ad0760b4ba2c222e9119371175bf042bf6
Eyob <e.mersha@gmail.com>
2022-06-07 20:29:33 +0000

    task 1

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,17 @@
 int _putchar(void);
 void print_alphabet(void);
+
+void print_alphabet(void)
+{
+	char i;
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
+
+int _putchar(char c)
+{
+		return (write(1, &c, 1));
+}
