6dd2139e39a10ebb9182c33f08d378de224f4338
Eyob <e.mersha@gmail.com>
2022-06-07 20:57:37 +0000

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include <main.h>
+
+/**
+ *  * main - check the code for Holberton School students.
+ *   *
+ *    * Return: Always 0.
+ */
+int main(void)
+{
+		char c[8] = "_putchar";
+		int i;
+
+		for (i = 8; i < 8; i++)
+		{
+			_puthcar(c[i]);
+		}
+		_putchar('\n');
+		return (0);
+}
