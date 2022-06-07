868158ce064d708b7afc94af60b50422a400e70f
Eyob <e.mersha@gmail.com>
2022-06-07 16:09:12 +0000

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,7 +7,13 @@
  *     */
 int main(void)
 {
+	char c[8] = "_putchar";
+	int i;
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(c[i]);
+	}
 	/*char arr[] = {'_putchar'}; */
-       	_putchar();
+       	_putchar('\n');
 		return (0);
 }
