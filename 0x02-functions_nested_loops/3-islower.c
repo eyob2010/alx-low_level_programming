3d73cd11b71faa443db32a465af1c5d03d108e26
Eyob <e.mersha@gmail.com>
2022-06-08 12:10:06 +0000

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include "ctype.h"
+/**
+ *  * main - check the code.
+ *   *
+ *    * Return: Always 0.
+*/
+int _islower(int c)
+{
+if (islower(c))
+{
+	return (1);
+}
+else
+{
+	return (0);
+}
+}
+
