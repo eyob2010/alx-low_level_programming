e31e69d68176682e0421e38a8bf57abe18338cbd
Eyob <e.mersha@gmail.com>
2022-06-08 17:37:38 +0000

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include "stdlib.h"
+/**
+ *  *  *  * _abs - computes the absolute value
+ *   *   *   * @n : Accept the number to be evaluated
+ *    *    *    *
+ *     *     *     * Return: the absolute value of a number
+*/
+int _abs(int n)
+{
+	return (abs(n));
+}
