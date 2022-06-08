f7e815f95c5b0c5652a68e7609dff400850b9cb2
Eyob <e.mersha@gmail.com>
2022-06-08 22:51:13 +0000

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -20,7 +20,7 @@ void print_to_98(int n)
 			}
 			else
 			{
-				printf("%d", i);
+				printf("%d ", i);
 			}
 		}
 	}
@@ -35,7 +35,7 @@ void print_to_98(int n)
 		}
 		else
 		{
-			printf("%d", i);
+			printf("%d ", i);
 		}
 	}
 	}
