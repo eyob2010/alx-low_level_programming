6f48d2cf560f0be82e351a9980344364937a6ce4
Eyob <e.mersha@gmail.com>
2022-06-09 20:18:56 +0000

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,12 @@
 #include "main.h"
 /**
  * * main - tests function that prints if integer is positive or negative
+ *
+ * parameter i : is a parameter accepts integer to be evaluated
+ *
  * * Return: 0
 */
-
+void positive_or_negative(int i);
 int main(void)
 {
 	int i = 0;
