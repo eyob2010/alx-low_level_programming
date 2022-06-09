18eabe7c2ef3214ff3708c4e2b4b0bbaf853ae8e
Eyob <e.mersha@gmail.com>
2022-06-09 13:23:50 +0000

    task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -15,13 +15,13 @@ int largest_number(int a, int b, int c)
 	if ((a > b) && (b > c))
 	{
 		largest = a;
+	}
 		else if ((b > a) && (a > c))
 		{
 			largest = b;
+		}
 			else
 				largest = c;
-		}
-	}
 
 			return (largest);
 }
