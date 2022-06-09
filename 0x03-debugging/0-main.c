e7f4796ce3eab7c32413fc11f13fc79a8094be66
Eyob <e.mersha@gmail.com>
2022-06-09 20:33:45 +0000

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,12 +4,7 @@
  *
  * * Return: 0
  *
- *  * positive_or_negative - is a function retunes postive
- *  or negative depending upon the input
- *
- *  * 'n' - is a parameter accepts integer to be evaluated
  */
-void positive_or_negative(int n);
 int main(void)
 {
 	int i = 0;
