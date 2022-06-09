9e79b76f952a40370f14ec53d0165696efb6ab37
Eyob <e.mersha@gmail.com>
2022-06-09 23:49:23 +0000

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,7 +6,11 @@
  */
 void print_numbers(void)
 {
-	_putchar('0'), _putchar('1'), _putchar('2'), _putchar('3'), _putchar('4'),
-		_putchar('5'), _putchar('6'), _putchar('7'),
-		_putchar('8'), _putchar('9'), _putchar('\n');
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
 }
