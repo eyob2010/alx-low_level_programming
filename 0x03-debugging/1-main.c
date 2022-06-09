177f675511e259cde5197dde43a105fe420431ee
Eyob <e.mersha@gmail.com>
2022-06-09 12:26:46 +0000

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,8 +10,9 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
-	i = 0;
 /**
+ * While - an infinite loop has been avoide
+ * i = 0;
  * while - with an infinite loop
  * i : doesn't increment and causes an infinite loop
  * while (i < 10)
@@ -19,7 +20,6 @@ int main(void)
  *		putchar(i);
  *	}
  *
- * *	printf("Infinite loop avoided! \\o/\n");
-*/
+ * */	printf("Infinite loop avoided! \\o/\n");
 	return (0);
 }
