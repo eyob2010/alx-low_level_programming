e4c3d8c5caf82bad893ee270428fc654d574aa41
Eyob <e.mersha@gmail.com>
2022-06-09 11:37:35 +0000

    task0

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,21 +1,25 @@
+/* @i : should increment inside the while loop before getting out */
 #include <stdio.h>
 
 /**
  * * main - causes an infinite loop
  * * Return: 0
-*/
+ * */
 
 int main(void)
 {
-	int i;
+	        int i;
 
-	printf("Infinite loop incoming :(\n");
-	i = 0;
-	while (i < 10)
-	{
-		putchar(i);
-		/* @i : should increment inside the while loop before getting out */
-	}
-	printf("Infinite loop avoided! \\o/\n");
-	return (0);
+		        printf("Infinite loop incoming :(\n");
+
+			        i = 0;
+
+				        while (i < 10)
+						        {
+								                putchar(i);
+										        }
+
+					        printf("Infinite loop avoided! \\o/\n");
+
+						        return (0);
 }
