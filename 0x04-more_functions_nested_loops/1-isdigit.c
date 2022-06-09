d13f9e8da92f7c9620eb3a0d45adaff05fb68147
Eyob <e.mersha@gmail.com>
2022-06-09 22:11:59 +0000

    task 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,5 +1,11 @@
 #include "main.h"
 #include "ctype.h"
+/**
+ * *  * _isdidgit - display 1 if the character is number other wise 0
+ *  * @c : a member parameter accepts input to be evaluated
+ * * *
+ *  * Return: On digit 1 other wise 0
+ */
 int _isdigit(int c)
 {
 	int i;
