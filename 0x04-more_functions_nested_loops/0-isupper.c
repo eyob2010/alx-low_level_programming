0b1801a1114b9998f803d7fe5cfc760161a6300e
Eyob <e.mersha@gmail.com>
2022-06-09 22:26:23 +0000

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,12 @@
 #include "main.h"
 #include "ctype.h"
+/**
+ *  *  *  *  *  * _isupper - a function that returns if
+ *  it's uppervase or not
+ *   *   *   *   * @c : Accept one character to be evaluated
+ *    * @c : accepts one integer
+ *      *     *     *     *     * Return: the result
+ */
 int _isupper(int c)
 {
 	int i;
