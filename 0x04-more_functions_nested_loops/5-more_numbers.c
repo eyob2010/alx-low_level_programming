34880c9a1b59b55e3288be6f62313d0a0c249be2
Eyob <e.mersha@gmail.com>
2022-06-10 00:09:32 +0000

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,12 @@
 #include "main.h"
+/**
+ *  *  *  *  *  *  * more_numbers - a function that prints
+ *   *  from 0 - 9 followed by new line
+ *    *      *      *     *     * Return: the result
+ *     */
 void more_numbers(void)
 {
-	char i;
+	char i, k;
 	int j = 0;
 
 	while (j <= 1)
@@ -15,9 +20,9 @@ void more_numbers(void)
 		}
 		else
 		{
-			for (k = 0; k <= 4; k++)
+			for (k = '0'; k <= '4'; k++)
 			{
-				_putchar(1), _putchar(k);
+				_putchar('1'), _putchar(k);
 			}
 		}
 		j++
