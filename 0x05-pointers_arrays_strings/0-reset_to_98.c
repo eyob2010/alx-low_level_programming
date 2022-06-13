d7545e50b7395117a13fe8a39b4fdad11ce9bb4e
Eyob <e.mersha@gmail.com>
2022-06-13 12:24:33 +0000

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,3 +1,10 @@
+/**
+ *  *  reset_to_98 - that takes a pointer to an
+ *  int as parameter and updates the value it points to to 98
+ *   *   *   * @n : Accept the character to be evaluated
+ *    *    *    *
+ *     *     *     * Return: On lower case 1 other wise 0
+ */
 #include "main.h"
 void reset_to_98(int *n)
 {
