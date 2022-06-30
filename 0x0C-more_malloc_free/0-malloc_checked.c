117a5a4fe7d0e61a6942aae4383ed16f66b58926
Eyob <e.mersha@gmail.com>
2022-06-30 20:48:26 +0000

    task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include"main.h"
+#include"stdlib.h"
+/**
+ *  * _malloc_checked -  a function that allocates memory using malloc
+ *   * @b: an argument to accept size of allocation
+ *    *
+ *     * Return: On success 1returnes pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *c;
+
+	c = malloc(b sizeof(int));
+	if (c == NULL)
+	{
+		return (98);
+	}
+	else
+	return (c);
+
+}
