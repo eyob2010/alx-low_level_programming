94aabe71ff1a280497ca59ba4eff4117815f7bc5
Eyob <e.mersha@gmail.com>
2022-07-05 19:23:22 +0000

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ *  * struct dog - a dog's info
+ *  * @name: stores the dog name
+ *  * @age: stores the dog's age
+ *  * @owner: stores the owners name
+ *      *
+ *       * Description: records a dog's detail
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
