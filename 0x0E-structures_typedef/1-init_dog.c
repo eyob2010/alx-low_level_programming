3f86c34d351b2ee69f761662890970e0431e2c1c
Eyob <e.mersha@gmail.com>
2022-07-05 20:31:12 +0000

    task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,9 +1,15 @@
 #include"stdlib.h"
 #include"dog.h"
+/**
+ *  *  * struct dog - a dog's info
+ *   *  * @name: stores the dog name
+ *    *  * @age: stores the dog's age
+ *     *  * @owner: stores the owners name
+ *      *      *
+ *       *       * Description: records a dog's detail
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog dd;
-	d = &dd;
 	d = malloc(sizeof(struct dog));
 	d->name = name;
 	d->age = age;
