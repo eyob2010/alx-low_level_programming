94ad22af8cc92cf0f4248660d4c10b02d66e7048
Eyob <e.mersha@gmail.com>
2022-07-15 22:40:48 +0000

    task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include "stddef.h"
+/**
+ * free_list - a function to free linked list memorey
+ * @head: the list
+ * Return: on success it returns void
+ */
+void free_list(list_t *head)
+{
+	list_t *temp_list;
+
+	while (head != NULL)
+	{
+		temp_list = head;
+		head = temp_list->next;
+		free(temp_list);
+	}
+}
