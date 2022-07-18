1cc2f3a8b3fd6a41cb74641c3896d9d07230e744
Eyob <e.mersha@gmail.com>
2022-07-18 20:46:58 +0000

    task 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -25,10 +25,5 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		ptr = ptr->next;
 	}
 	ptr->next = temp;
-	ptr = *head;
-	while (ptr != NULL)
-	{
-		ptr = ptr->next;
-	}
-	return (ptr);
+	return (temp);
 }
