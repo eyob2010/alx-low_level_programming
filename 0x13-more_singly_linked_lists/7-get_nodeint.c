fab5ea8a078be4d729d577239903416f035b4427
Eyob <e.mersha@gmail.com>
2022-07-18 21:00:49 +0000

    task 7

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,22 @@
+#include"stdlib.h"
+#include"lists.h"
+/**
+ * get_nodeint_at_index - a function returns the node at specific index
+ * @head: accepts the address of the head of the node
+ * @index: an argument which accepts the specific index
+ * Return: on success it return specified node of the index
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i, count = 0;
+
+	i = index;
+	listint_t *ptr;
+
+	ptr = head;
+	while (count != i)
+	{
+		ptr = ptr->next;
+	}
+	return (ptr);
+}
