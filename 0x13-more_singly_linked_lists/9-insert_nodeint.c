c9f28b80edad339feb057fbf59e0f36495dc2dc7
Eyob <e.mersha@gmail.com>
2022-07-19 19:38:40 +0000

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,7 +11,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int idx1 = idx;
 	int data = n;
-	unsigned int count = 0;
 	listint_t *ptr, *ptr2;
 
 	ptr = *head;
@@ -23,17 +22,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		ptr = ptr->next;
 		idx1--;
-		count++;
 	}
-	if (idx >= count)
-	{
-		ptr = NULL;
-		return (ptr);
-	}
-	else
-	{
-		ptr2->next = ptr->next;
-		ptr->next = ptr2;
-		return (ptr2);
-	}
+	ptr2->next = ptr->next;
+	ptr->next = ptr2;
+	return (ptr2);
 }
