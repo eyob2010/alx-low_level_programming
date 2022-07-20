3bef5d0a38baa53f3591afdd62dc11ef3906fc3d
Eyob <e.mersha@gmail.com>
2022-07-20 14:42:31 +0000

    task 14

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,26 @@
+#include"stdlib.h"
+#include"lists.h"
+/**
+ * find_listint_loop - a function which
+ * identifi a loop in a linked list
+ * @head: a pointer to the head of an argument;
+ * Return: when find a oop returns a pointer to the
+ * finding oytherwise a returnes NULL
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *current = head;
+	listint_t *prev = head;
+
+	while (prev && current && current->next)
+	{
+		prev = prev->next;
+		current = current->next->next;
+		if (prev == current)
+		{
+			return (prev);
+		}
+	}
+	prev = NULL;
+	return (prev);
+}
