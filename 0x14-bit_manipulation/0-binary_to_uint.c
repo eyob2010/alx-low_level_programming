326d280e85f9c3d91d14a7747b73f1919a3b309b
Eyob <e.mersha@gmail.com>
2022-07-21 17:22:09 +0000

    task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,56 @@
+#include"main.h"
+#include"stdlib.h"
+#include"math.h"
+#include"ctype.h"
+/**
+ * binary_to_uint - a function that converts a
+ * binary number to an unsigned int
+ * @s: a pinter to the binary string
+ * Return: on success it returns the decimal
+ * equivalent else, will return 0
+ */
+int digits_only(const char *s);
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int dec = 0, we = 1, rem, binary, n;
+
+	if (b == NULL)
+	{
+		return (dec);
+	}
+	n = digits_only(b);
+	if (n == 0)
+	{
+		return (dec);
+	}
+	else if (n == 1)
+	{
+		binary = atoi(b);
+		while (binary != 0)
+		{
+			rem = binary % 10;
+			dec = dec + (rem * we);
+			binary = binary / 10;
+			we = we * 2;
+		}
+		return (dec);
+	}
+	return (dec);
+}
+/**
+ * digits_only - a function to identify if there
+ * is a character in a number.
+ * @s: a pointer that holds
+ * the string to be tested
+ * Return: on success it returns 0 if not digit
+ * and 1 if digit
+ */
+int digits_only(const char *s)
+{
+	while (*s)
+	{
+		if (isdigit(*s++) == 0)
+			return (0);
+	}
+	return (1);
+}
