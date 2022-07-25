37ece39e9b32b5cf3638276d8320376133a24ee7
Eyob <e.mersha@gmail.com>
2022-07-25 23:03:38 +0000

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,17 +13,24 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t fd;
-	char *buf = NULL;
-
-	fd = open("%d", *filename, O_RDONLY);
-	if (fd == -1)
+	if (filename == NULL)
 	{
 		return (0);
-		exit(1);
 	}
-	read(fd, buf, letters - 1);
-	close(fd);
-	printf("%s\n", buf);
+	else
+	{
+		ssize_t fd;
+		char *buf = NULL;
+
+		fd = open("%d", *filename, O_RDONLY);
+		if (fd == -1)
+		{
+			return (0);
+			exit(1);
+		}
+		read(fd, buf, letters - 1);
+		close(fd);
+		printf("%s\n", buf);
+	}
 	return (letters - 1);
 }
